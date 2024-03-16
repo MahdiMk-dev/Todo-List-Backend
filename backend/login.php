aa6134712b075733e7cdd146b678641b164eb513
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-03-16 08:06:30 +0200

    login and connection update

--- backend/login.php
+++ backend/login.php
@@ -1,33 +1,32 @@
-<?php
-include('connection.php');
+    <?php
+    include('connection.php');
 
-$email = $_POST['name'];
-$password = $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$query = $mysqli->prepare('select id,email,password,name
-from users
-where email=? or name=?');
-$query->bind_param('ss', $email,$email);
-$query->execute();
-$query->store_result();
-$query->bind_result($id, $email, $hashed_password, $name);
-$query->fetch();
-$num_rows = $query->num_rows();
+    $query = $mysqli->prepare('select id,email,password,name
+    from users
+    where email=?');
+    $query->bind_param('s', $email);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($id, $email, $hashed_password);
+    $query->fetch();
+    $num_rows = $query->num_rows();
 
-if ($num_rows == 0) {
-    $response['status'] = "user not found";
-} else {
-
-    if (password_verify($password, $hashed_password)) {
-        $response['status'] = "logged_in";
-        $response['user_id'] = $id;
-        $response['name'] = $name;
-        $response['email'] = $email;
+    if ($num_rows == 0) {
+        $response['status'] = "user not found";
     } else {
-        $response['status'] = "incorrect credentials";
-    }
-}
-header('Content-Type: application/json');
 
-echo json_encode($response);
-?>
+        if (password_verify($password, $hashed_password)) {
+            $response['status'] = "logged_in";
+            $response['user_id'] = $id;
+            $response['email'] = $email;
+        } else {
+            $response['status'] = "incorrect credentials";
+        }
+    }
+    header('Content-Type: application/json');
+
+    echo json_encode($response);
+    ?>
